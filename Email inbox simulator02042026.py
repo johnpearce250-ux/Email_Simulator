e523330bbde8c2ddc5820ae219f503bdd41a4353
John Pearce <johnpearce250@gmail.com>
2026-04-27 10:22:27 -0500

    Changed __init__for functionality

--- Email inbox simulator02042026.py
+++ Email inbox simulator02042026.py
@@ -29,10 +29,10 @@ class Email:
 
 
 class User:
-    def __init__ (self,name, inbox):
+    def __init__ (self, name):
         """Initial a User object with a name and inbox"""
         self.name = name
-        self.inbox= Inbox()   
+        self.inbox = Inbox()
     
     def send_email(self, receiver, subject, body):
         """Create an Email object and send it to the receiver."""
@@ -65,6 +65,7 @@ class Inbox:
     def list_emails(self):
         if not self.emails:
             print("Your inbox is empty.\n")
+            return
         print('\nYour Emails:')
         for i, email in enumerate(self.emails, start=1):
             print(f'{i}. {email}')
@@ -73,30 +74,34 @@ class Inbox:
         """Display the full details of the email at the given index."""
         if not self.emails:
             print("Inbox is empty.\n")
+            return
         actual_index = index - 1
         if actual_index < 0 or actual_index >= len(self.emails):
             print("Invalid email number.\n")
+            return
         self.emails[actual_index].display_full_email()
             
     def delete_email(self, index):
         """Delete the email at the given index from the inbox."""
         if not self.emails:
             print("Inbox is empty.\n")
+            return
         actual_index = index - 1
         if actual_index < 0 or actual_index >= len(self.emails):
             print("Invalid email number.\n")
+            return
         del self.emails[actual_index]
         print("Email deleted.\n")
     
-    def main():
-        tory = User("Tory")
-        ramy= User("Ramy")
-        tory.send_email(ramy, "Hello", "Hi Ramy, just saying hello!")
-        ramy.send_email(tory, "Re: Hello", "Hi Tory, hope you are fine.")
-        ramy.check_inbox()
-        ramy.read_email(1)
-        ramy.delete_email(1)
-        ramy.check_inbox()
+def main():
+    tory = User("Tory")
+    ramy = User("Ramy")
+    tory.send_email(ramy, "Hello", "Hi Ramy, just saying hello!")
+    ramy.send_email(tory, "Re: Hello", "Hi Tory, hope you are fine.")
+    ramy.check_inbox()
+    ramy.read_email(1)
+    ramy.delete_email(1)
+    ramy.check_inbox()
 
 if __name__ == "__main__":
     main()
